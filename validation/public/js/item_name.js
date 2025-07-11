a5a8be09dba8387906e3f300059a9a3868b20959
imadhubala27 <imadhubala27@gmail.com>
2025-07-11 16:28:21 +0530

    autoselect for automation

--- validation/public/js/item_name.js
+++ validation/public/js/item_name.js
@@ -9,14 +9,19 @@ frappe.ui.form.on('Item', {
     item_code: function(frm) {
         if (frm.doc.custom_automate) {
             console.log("ItemCode trigger activated and custom_automate is disabled");
-            check_automation_enabled(frm, function(is_enabled) {
-                console.log("Automation check result:", is_enabled);
-                if (is_enabled) {
+
+            check_item_automation_settings(function(settings) {
+                console.log("Automation Settings:", settings);
+                
+                if (settings.enable_item_automation && !settings.item_code_automation) {
                     const formatted_name = format_name(frm.doc.item_code);
                     console.log("Formatted Name:", formatted_name);
                     frm.set_value('item_code', formatted_name);
+                } else {
+                    console.log("Skipping formatting. Because either enable_item_automation is disabled or item_code_automation is enabled.");
                 }
             });
+
         } else {
             console.log("custom_automate is enabled. Skipping Item Code trigger.");
         }
@@ -24,14 +29,19 @@ frappe.ui.form.on('Item', {
     item_name: function(frm) {
         if (frm.doc.custom_automate) {
             console.log("ItemCode trigger activated and custom_automate is disabled");
-            check_automation_enabled(frm, function(is_enabled) {
-                console.log("Automation check result:", is_enabled);
-                if (is_enabled) {
+
+            check_item_automation_settings(function(settings) {
+                console.log("Automation Settings:", settings);
+                
+                if (settings.enable_item_automation) {
                     const formatted_name = format_name(frm.doc.item_name);
                     console.log("Formatted Name:", formatted_name);
                     frm.set_value('item_name', formatted_name);
+                } else {
+                    console.log("Skipping formatting. Because either enable_item_automation is disabled or item_name is enabled.");
                 }
             });
+
         } else {
             console.log("custom_automate is enabled. Skipping Item Code trigger.");
         }
@@ -39,14 +49,19 @@ frappe.ui.form.on('Item', {
     description: function(frm) {
         if (frm.doc.custom_automate) {
             console.log("ItemCode trigger activated and custom_automate is disabled");
-            check_automation_enabled(frm, function(is_enabled) {
-                console.log("Automation check result:", is_enabled);
-                if (is_enabled) {
+
+            check_item_automation_settings(function(settings) {
+                console.log("Automation Settings:", settings);
+                
+                if (settings.enable_item_automation) {
                     const formatted_name = format_name(frm.doc.description);
                     console.log("Formatted Name:", formatted_name);
                     frm.set_value('description', formatted_name);
+                } else {
+                    console.log("Skipping formatting. Because either enable_item_automation is disabled or description is enabled.");
                 }
             });
+
         } else {
             console.log("custom_automate is enabled. Skipping Item Code trigger.");
         }
@@ -83,17 +98,32 @@ function format_name(name) {
     return formattedName;
 }
 
-function check_automation_enabled(frm, callback) {
+function check_item_automation_settings(callback) {
     frappe.call({
         method: 'frappe.client.get_single_value',
         args: {
             doctype: 'Settings for Automation',
             field: 'enable_item_automation'
         },
-        callback: function(response) {
-            const is_enabled = response.message ? response.message : false;
-            console.log("Automation enabled?", is_enabled);
-            if (callback) callback(is_enabled);
+        callback: function(response1) {
+            const enable_item_automation = response1.message ? response1.message : 0;
+
+            frappe.call({
+                method: 'frappe.client.get_single_value',
+                args: {
+                    doctype: 'Settings for Automation',
+                    field: 'item_code_automation'
+                },
+                callback: function(response2) {
+                    const item_code_automation = response2.message ? response2.message : 0;
+
+                    callback({
+                        enable_item_automation: enable_item_automation,
+                        item_code_automation: item_code_automation
+                    });
+                }
+            });
         }
     });
 }
+
--- validation/validation/doctype/settings_for_automation/settings_for_automation.js
+++ validation/validation/doctype/settings_for_automation/settings_for_automation.js
@@ -1,8 +1,20 @@
-// Copyright (c) 2025, Ameer Babu and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Settings for Automation", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Settings for Automation", {
+    refresh(frm) {
+        if (!frm.doc.__unsaved) {
+            frm.set_value('enable_item_automation', 1);
+            frm.set_value('enable_bank_automation', 1);
+            frm.set_value('custom_customer_group', 1);
+            frm.set_value('custom_supplier_group', 1);
+            frm.set_value('enable_employee_automation', 1);
+            frm.set_value('enable_brand_automation', 1);
+            frm.set_value('custom_terms_and_conditions', 1);
+            frm.set_value('custom_payment_term', 1);
+            frm.set_value('custom_payment_term_template', 1);
+            frm.set_value('enable_supplier_automation', 1);
+            frm.set_value('enable_item_group_automation', 1);
+            frm.set_value('enable_contact_automation', 1);
+            frm.set_value('enable_address_automation', 1);
+            frm.set_value('enable_customer_automation', 1);
+        }
+    },
+});
